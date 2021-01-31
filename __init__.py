44aefe38c30c7b48a100d5033089863dba91e393
Casey S Zduniak <32501098+csz5024@users.noreply.github.com>
2021-01-31 17:26:00 -0500

    finished!

--- __init__.py
+++ __init__.py
@@ -1,16 +1,24 @@
-import ipfsApi
+import ipfsapi
 import webbrowser
 import os
 import hashlib
-import web3
+from web3.auto.infura import w3
 
+#need to set the project id and project secret ENVARS from infura
+
+#contract address
+#0x936d7AAc1ca7424f1559eF06eDF818A3FeD57376
 if __name__ == '__main__':
-    api = ipfsApi.Client(host='https://ipfs.infura.io', port=5001)
+    api = ipfsapi.Client(host='https://ipfs.infura.io', port=5001)
     api.get('QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP')
 
+    contract_abi = '[{"inputs":[{"internalType":"bytes16","name":"num","type":"bytes16"}],"name":"validate","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}]'
 
+    #print(w3.eth.get_block(11759489))
+    myContract = w3.eth.contract(address='0x936d7AAc1ca7424f1559eF06eDF818A3FeD57376', abi=contract_abi)
+    #print(myContract.functions.validate(hashlib.md5(b"QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP").hexdigest()).call())
 
-    # verify this against eth smart contract
-    print(hashlib.md5(b"QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP").hexdigest())
-
-    webbrowser.open('file://' + os.path.realpath('QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP'), new=2)
+    #If the ipfs CID matches the md5 endcoded hash in the ethereum smart contract, then open the document.
+    if(myContract.functions.validate(hashlib.md5(b"QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP").hexdigest()).call()):
+        webbrowser.open('file://' + os.path.realpath('QmPkesxLKThDvUrdPxgwWu7LdTbRgUghMpKE99q5SS4aAP'), new=2)
+        print("This document has been verified using the Ethereum Blockchain")
